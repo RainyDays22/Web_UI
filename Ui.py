6eed668e65ff54b3ccf36bd92c582b0e49045db7
Colleen Polka <colleenpolka@Colleens-MacBook-Air.local>
2022-03-26 18:59:50 -0400

    base model

--- Ui.py
+++ Ui.py
@@ -1,6 +1,28 @@
-from tkinter import *
-window=Tk()
+from turtle import *
+screen=Screen()
 # add widgets here
+
+class Question:
+    def __init__(self):
+        screen = Screen()
+
+    def title(self, title_text):
+        lbl = Label(screen, text = str(title_text), )
+        return lbl
+
+    def option1(self, image, text):
+        pass
+
+    def option2(self, image, text):
+        pass
+
+    def option3(self, image, text):
+        pass
+
+    def option4(self, image, text):
+        pass
+
+    
 
 window.title('Hello Python')
 window.geometry("300x200+10+20")
