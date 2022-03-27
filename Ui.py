7202485f038800131b080528946a003ddee58be1
Colleen Polka <colleenpolka@Colleens-MacBook-Air.local>
2022-03-26 23:21:49 -0400

    more stuff

--- Ui.py
+++ Ui.py
@@ -10,7 +10,18 @@ from tkinter import *
 
 questions = {1:['question 1 title!', 'rats', 'cats', 'hats', 'mats'],
             2:['question 2 title!', 'option 1', 'option 2', 'option 3', 'option 4']}
-print(questions[1][0],questions[1][1])
+
+def add_A(self):
+    self.answers.append('A')
+
+def add_B(self):
+    self.answers.append('B')
+
+def add_C(self):
+    self.answers.append('C')
+
+def add_D(self):
+    self.answers.append('D')
 
 class Quiz:
 
@@ -22,26 +33,38 @@ class Quiz:
         self.enter_btn = Button(self.frame, text="Enter quiz", command=self.next_question)
         self.enter_btn.pack()
         self.answers = []
+        self.counter = 1
 
-    def track_answers(self, text):
-        self.answers.append(text)
+    def question_counter(self):
+        return self.counter
+
+    def track_answers(self):
+        return self.answers
     
     def question(self, title, option1, option2, option3, option4):
         self.title = Label(self.frame, text = title)
         self.title.pack()
-        self.option1 = Button(self.frame, text = option1, command = self.track_answers('A'))
+        self.option1 = Button(self.frame, text = option1, command = add_A)
         self.option1.pack()
-        self.option2 = Button(self.frame, text = option2, command = self.track_answers('B'))
+        self.option2 = Button(self.frame, text = option2, command = add_B)
         self.option2.pack()
-        self.option3 = Button(self.frame, text = option3, command = self.track_answers('C'))
+        self.option3 = Button(self.frame, text = option3, command = add_C)
         self.option3.pack()
-        self.option4 = Button(self.frame, text = option4, command = self.track_answers('D'))
+        self.option4 = Button(self.frame, text = option4, command = add_D)
         self.option4.pack()
+        print('we are here')
+        print(self.track_answers())
+        self.next_question()
 
     def next_question(self):
         for i in self.frame.winfo_children():
             i.destroy()
-        self.question(questions[1][0], questions[1][1], questions[1][2], questions[1][3], questions[1][4])
+        q = self.counter
+        if self.counter < 2:
+            self.question(questions[q][0], questions[q][1], questions[q][2], questions[q][3], questions[q][4])
+            self.counter += 1
+        else:
+            pass #end result
 
     
 
