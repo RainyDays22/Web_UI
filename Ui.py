89a7d6a51e1418f34653b9913c29d990cc025fd7
Colleen Polka <colleenpolka@Colleens-MacBook-Air.local>
2022-03-27 01:13:33 -0400

    images

--- Ui.py
+++ Ui.py
@@ -97,11 +97,32 @@ class Quiz:
         title = Label(text = 'You are a ' + str(result) + '!')
         title.pack()
         if result == 'squirrel':
-            squirrel = Image.open('x')
+            urllib.request.urlretrieve('https://i.imgur.com/PEIFopL.jpg', 'squirrel.jpeg')
+            squirrel_pic = Image.open('squirrel.jpeg')
+            squirrel = ImageTk.PhotoImage(squirrel_pic)
+            label = Label(image = squirrel)
+            label.image = squirrel
+            label.pack()
+            desc = Label(text = 'add a description of u as squirrel here')
+            desc.pack()
         if result == 'frog':
-            pass
+            urllib.request.urlretrieve('https://i.imgur.com/McbobUQ.jpeg', 'frog.jpeg')
+            frog_pic = Image.open('frog.jpeg')
+            frog = ImageTk.PhotoImage(frog_pic)
+            label = Label(image = frog)
+            label.image = frog
+            label.pack()
+            desc = Label(text = 'add a description of u as frog here')
+            desc.pack()
         if result == 'Swem therapy dog':
-            pass
+            urllib.request.urlretrieve('https://events.wm.edu/images/event_uploads/sjgilliam_2018_10_04_14_43_59.jpg', 'dog.jpeg')
+            dog_pic = Image.open('dog.jpeg')
+            dog = ImageTk.PhotoImage(dog_pic)
+            label = Label(image = dog)
+            label.image = dog
+            label.pack()
+            desc = Label(text = 'add a description of u as dog here')
+            desc.pack()
         if result == 'blue heron':
             urllib.request.urlretrieve('https://www.wm.edu/as/biology/planttour/_photosets/crimdell/heron.jpg', 'heron.jpeg')
             heron_pic = Image.open('heron.jpeg')
--- even_more_tests.py
+++ even_more_tests.py
@@ -1,40 +1,36 @@
-from tkinter import *
-class MyWindow:
-    def __init__(self, win):
-        self.lbl1=Label(win, text='First number')
-        self.lbl2=Label(win, text='Second number')
-        self.lbl3=Label(win, text='Result')
-        self.t1=Entry(bd=3)
-        self.t2=Entry()
-        self.t3=Entry()
-        self.btn1 = Button(win, text='Add')
-        self.btn2=Button(win, text='Subtract')
-        self.lbl1.place(x=100, y=50)
-        self.t1.place(x=200, y=50)
-        self.lbl2.place(x=100, y=100)
-        self.t2.place(x=200, y=100)
-        self.b1=Button(win, text='Add', command=self.add)
-        self.b2=Button(win, text='Subtract')
-        self.b2.bind('<Button-1>', self.sub)
-        self.b1.place(x=100, y=150)
-        self.b2.place(x=200, y=150)
-        self.lbl3.place(x=100, y=200)
-        self.t3.place(x=200, y=200)
-    def add(self):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1+num2
-        self.t3.insert(END, str(result))
-    def sub(self, event):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1-num2
-        self.t3.insert(END, str(result))
-
-window=Tk()
-mywin=MyWindow(window)
-window.title('Hello Python')
-window.geometry("400x300+10+10")
-window.mainloop()
+if result == 'squirrel':
+            urllib.request.urlretrieve('https://i.imgur.com/PEIFopL.jpg', 'squirrel.jpeg')
+            squirrel_pic = Image.open('squirrel.jpeg')
+            squirrel = ImageTk.PhotoImage(squirrel_pic)
+            label = Label(image = squirrel)
+            label.image = squirrel
+            label.pack()
+            desc = Label(text = 'add a description of u as squirrel here')
+            desc.pack()
+        if result == 'frog':
+            urllib.request.urlretrieve('https://i.imgur.com/McbobUQ.jpeg', 'frog.jpeg')
+            frog_pic = Image.open('frog.jpeg')
+            frog = ImageTk.PhotoImage(frog_pic)
+            label = Label(image = frog)
+            label.image = frog
+            label.pack()
+            desc = Label(text = 'add a description of u as frog here')
+            desc.pack()
+        if result == 'Swem therapy dog':
+            urllib.request.urlretrieve('https://events.wm.edu/images/event_uploads/sjgilliam_2018_10_04_14_43_59.jpg', 'dog.jpeg')
+            dog_pic = Image.open('dog.jpeg')
+            dog = ImageTk.PhotoImage(dog_pic)
+            label = Label(image = dog)
+            label.image = dog
+            label.pack()
+            desc = Label(text = 'add a description of u as dog here')
+            desc.pack()
+        if result == 'blue heron':
+            urllib.request.urlretrieve('https://www.wm.edu/as/biology/planttour/_photosets/crimdell/heron.jpg', 'heron.jpeg')
+            heron_pic = Image.open('heron.jpeg')
+            heron = ImageTk.PhotoImage(heron_pic)
+            label = Label(image = heron)
+            label.image = heron
+            label.pack()
+            desc = Label(text = 'add a description of u as heron here')
+            desc.pack()
